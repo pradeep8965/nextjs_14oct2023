e05d6b15f295783b26fe3e51223b426d44367d37
“misterpp143@gmail.comgit config --global user.email misterpp143@gmail.comgit config  credential.helper ‘store’git config --global user.name “misterpp143@gmail.com <misterpp143@gmail.com>
2023-10-21 14:36:57 +0530

    c1

---
+++ src/app/components/Header.js
@@ -0,0 +1,16 @@
+//1. Import Area
+import React from 'react'
+
+
+//2. Define Area 
+function Header() {
+    return (
+        <header>
+            
+        </header>
+    
+  )
+}
+
+//3. Export Area
+export default Header;
